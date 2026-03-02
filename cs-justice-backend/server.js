d73bd1cc826494ef325077149d72ce8ec3946302
JacksonMS <controle_espacial@outlook.com>
2026-03-02 11:01:23 -0300

    pequena correção server + index

--- cs-justice-backend/server.js
+++ cs-justice-backend/server.js
@@ -33,6 +33,7 @@ app.use(helmet({
 app.use(cors());
 app.use(express.json({ limit: '50mb' })); 
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.static(path.join(__dirname, 'static')))
 
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
 
