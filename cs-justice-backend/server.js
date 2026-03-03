7f8c59b0bd6e9aa5fd71d398023aed233b949626
JacksonMS <controle_espacial@outlook.com>
2026-03-03 13:46:26 -0300

    bug no avatar do login corrigido

--- cs-justice-backend/server.js
+++ cs-justice-backend/server.js
@@ -54,8 +54,8 @@ app.use(session({
     proxy: true, 
     cookie: { 
         maxAge: 24 * 60 * 60 * 1000, 
-        secure: true,   
-        sameSite: 'none' 
+        secure: process.env.NODE_ENV === 'production',   
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax' 
     }
 }));
 
